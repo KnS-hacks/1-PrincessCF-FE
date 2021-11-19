50931f196d3680f678ebff2867b3b1ae86594626
minsgy <alstjrchl97@naver.com>
2021-11-20 03:38:09 +0900

    feat: proxy setting

--- src/api/MusicApi.jsx
+++ src/api/MusicApi.jsx
@@ -1,6 +1,6 @@
 import api from './api';
 
 export const PostMusic = async (param) =>
-  await api({ url: '/music/list', method: 'post', data: param });
+  await api({ url: '/music/list', type: 'post', data: param });
 
 export const GetMusicList = async () => await api({ url: '/music/list', method: 'get' });
